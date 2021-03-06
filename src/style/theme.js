fbbd27c8114752f1ee78590bff438c32e158780c
Damian Kalka <kalka762@gmail.com>
2021-03-06 16:24:45 +0100

    add theme variables

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,3 +1,14 @@
-const theme = {};
+const theme = {
+  color: {
+    primary: '#639aa6',
+    secondary: '#306073',
+    pink: '#f2385a',
+    background: '#fff',
+    border: '#D5D4D4',
+    container: '#ebebeb',
+    darkFont: '#595454',
+    fairFont: '#ebebeb',
+  },
+};
 
 export default theme;
