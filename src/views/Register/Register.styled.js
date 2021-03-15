217f2b540712fb09656d6f1e42b3f863ad6e40c6
Damian Kalka <kalka762@gmail.com>
2021-03-15 21:05:17 +0100

    update global styles

--- src/views/Register/Register.styled.js
+++ src/views/Register/Register.styled.js
@@ -2,15 +2,11 @@ import StyledContainer from 'components/molecules/Container/Container';
 import styled from 'styled-components';
 
 const StyledRegisterPage = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
   height: 100%;
   width: 100%;
 
   ${StyledContainer} {
-    margin: 60px 0;
+    margin: 60px auto;
   }
 
   .register {
